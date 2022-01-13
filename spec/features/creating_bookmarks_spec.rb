6afe1fbf097b779de7efcfd281fdebb52ef8a94c
OrganicAC <anthonychaplin17@gmail.com>
2022-01-13 16:43:07 +0000

    completed step 11

--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -2,8 +2,9 @@ feature 'Adding new bookmarks' do
   scenario 'A user can add a new bookmark' do
     visit '/bookmarks/new'
     fill_in 'url', with: 'http://www.youtube.com/'
+    fill_in 'title', with: 'Youtube'
     click_on 'Submit'
 
-    expect(page).to have_content 'http://www.youtube.com/'
+    expect(page).to have_link('Youtube', href: 'http://www.youtube.com/')
   end
-end
+end 
